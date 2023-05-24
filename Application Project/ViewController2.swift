a9ff354c06c1f2125f948d803f7cb988a1487a13
Harshid Vaddoriya <harshidpatel388@gmail.com>
2023-05-24 11:02:22 +0530

    123

---
+++ Application Project/ViewController2.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController2.swift
+//  Application Project
+//
+//  Created by HARSHID PATEL on 13/04/23.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBOutlet weak var imgView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        imgView.layer.cornerRadius = imgView.frame.size.width/2
+    }
+    func navigation(){
+        let navigate = storyboard?.instantiateViewController(withIdentifier: "TabBarController") as! TabBarController
+        navigationController?.pushViewController(navigate, animated: true)
+    }
+    
+    @IBAction func saveButtonAction(_ sender: Any) {
+        navigation()
+    }
+    
+    @IBAction func notNowButtonAction(_ sender: Any) {
+        navigation()
+    }
+    
+}
